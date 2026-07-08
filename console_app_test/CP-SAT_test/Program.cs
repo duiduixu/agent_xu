e1ccdb11949bb3a61c6303c8775dc3dd944528e3
xuzy <xzy137@126.com>
2026-07-08 17:41:38 +0800

    CP-SAT 求解器示例

---
+++ console_app_test/CP-SAT_test/Program.cs
@@ -0,0 +1,31 @@
+﻿
+using System;
+using Google.OrTools.Sat;
+
+
+CpModel model = new CpModel();
+int numVars = 3;
+IntVar x = model.NewIntVar(0, numVars, "x");
+IntVar y = model.NewIntVar(0, numVars, "y");
+IntVar z = model.NewIntVar(0, numVars, "z");
+model.Add(x != y);
+model.Add(y != z);
+model.Add(x == z);
+CpSolver solver = new CpSolver();
+CpSolverStatus status = solver.Solve(model);
+
+if (status == CpSolverStatus.Optimal || status == CpSolverStatus.Feasible)
+{
+    Console.WriteLine("x = " + solver.Value(x));
+    Console.WriteLine("y = " + solver.Value(y));
+    Console.WriteLine("z = " + solver.Value(z));
+}
+else
+{
+    Console.WriteLine("No solution found.");
+}
+
+
+
+
+
