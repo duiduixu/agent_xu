d6bd56b3f123b6a46fc06faa98ff412c15a35227
xuzy <xzy137@126.com>
2026-07-31 18:08:35 +0800

    1

--- console_app_test/AAATest/Program.cs
+++ console_app_test/AAATest/Program.cs
@@ -1,4 +1,13 @@
 ﻿Console.WriteLine("Hello, World!");
 
 
-Console.WriteLine("enum:" + ImportModeEnum.CreateTableOnly);
+// Console.WriteLine("enum:" + ImportModeEnum.CreateTableOnly);
+
+var values = new Dictionary<String,String?>(StringComparer.OrdinalIgnoreCase);
+values["abc"] = "111";
+foreach(var value in values)
+{
+    Console.WriteLine(value);
+}
+Console.WriteLine(values);
+
